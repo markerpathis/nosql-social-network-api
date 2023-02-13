f74dcb7cf6ecbf44ece3857ca0732f4d8d8249c3
Parker Mathis <parkerjmathis@gmail.com>
2023-02-12 16:21:40 -0800

    Added newFriend route

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
 
-const { getUsers, getSingleUser, postUser, updateUser, deleteUser } = require("../../controllers/userController");
+const { getUsers, getSingleUser, postUser, updateUser, deleteUser, newFriend } = require("../../controllers/userController");
 
 // /api/users
 router.route("/").get(getUsers).post(postUser);
 
 // /api/users/:userId
 router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+
+router.route("/:userId/friends/:friendId").post(newFriend);
 
 module.exports = router;
