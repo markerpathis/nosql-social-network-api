d4dad431fb7706405022ef74ff4f0e0ffc714d0c
Parker Mathis <parkerjmathis@gmail.com>
2023-02-12 16:26:25 -0800

    Added removeFriend route

--- controllers/userController.js
+++ controllers/userController.js
@@ -44,4 +44,9 @@ module.exports = {
       .then((user) => (!user ? res.status(404).json({ message: "No user found with that ID :(" }) : res.json(user)))
       .catch((err) => res.status(500).json(err));
   },
+  removeFriend(req, res) {
+    User.findOneAndUpdate({ _id: req.params.userId }, { $pull: { friends: req.params.friendId } }, { runValidators: true, new: true })
+      .then((user) => (!user ? res.status(404).json({ message: "No user found with that ID :(" }) : res.json(user)))
+      .catch((err) => res.status(500).json(err));
+  },
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-const { getUsers, getSingleUser, postUser, updateUser, deleteUser, newFriend } = require("../../controllers/userController");
+const { getUsers, getSingleUser, postUser, updateUser, deleteUser, newFriend, removeFriend } = require("../../controllers/userController");
 
 // /api/users
 router.route("/").get(getUsers).post(postUser);
@@ -8,6 +8,6 @@ router.route("/").get(getUsers).post(postUser);
 // /api/users/:userId
 router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
 
-router.route("/:userId/friends/:friendId").post(newFriend);
+router.route("/:userId/friends/:friendId").post(newFriend).delete(removeFriend);
 
 module.exports = router;
