ae1d4fdca34445e03554a5c4184fe8e6cea920f0
Parker Mathis <parkerjmathis@gmail.com>
2023-02-12 17:49:55 -0800

    Added updateThought and deleteThought routes

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -29,4 +29,28 @@ module.exports = {
       .then((thought) => (!thought ? res.status(404).json({ message: "No thought with that ID" }) : res.json(thought)))
       .catch((err) => res.status(500).json(err));
   },
+  updateThought(req, res) {
+    Thought.findOneAndUpdate({ _id: req.params.thoughtId }, { $set: req.body }, { runValidators: true, new: true })
+      .then((thought) => (!thought ? res.status(404).json({ message: "No thought with that ID" }) : res.json(thought)))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  deleteThought(req, res) {
+    Thought.findOneAndRemove({ _id: req.params.thoughtId })
+      .then((thought) => {
+        if (!thought) {
+          return res.status(404).json({ message: "No thought with that ID" });
+        } else {
+          User.findOneAndUpdate({ thoughts: req.params.thoughtId }, { $pull: { thoughts: req.params.thoughtId } }, { new: true }).then(() => {
+            return res.json({ message: "Thought deleted and associated user updated" });
+          });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
 };
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 
-const { postThought, getThoughts, getSingleThought } = require("../../controllers/thoughtController");
+const { postThought, getThoughts, getSingleThought, updateThought, deleteThought } = require("../../controllers/thoughtController");
 
 // /api/thoughts
 router.route("/").get(getThoughts).post(postThought);
 
 // /api/thoughts/:thoughtId
-router.route("/:thoughtId").get(getSingleThought);
+router.route("/:thoughtId").get(getSingleThought).put(updateThought).delete(deleteThought);
 
 module.exports = router;
