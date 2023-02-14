ea26bbb6c0e0cb90aed699968e73d6433a44cc4b
Parker Mathis <parkerjmathis@gmail.com>
2023-02-13 17:52:09 -0800

    Added postReaction and deleteReaction routes

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -53,4 +53,14 @@ module.exports = {
         res.status(500).json(err);
       });
   },
+  postReaction(req, res) {
+    Thought.findOneAndUpdate({ _id: req.params.thoughtId }, { $addToSet: { reactions: req.body } }, { runValidators: true, new: true })
+      .then((thought) => (!thought ? res.status(404).json({ message: "No thought found with that ID :(" }) : res.json(thought)))
+      .catch((err) => res.status(500).json(err));
+  },
+  deleteReaction(req, res) {
+    Thought.findOneAndUpdate({ _id: req.params.thoughtId }, { $pull: { reactions: { reactionId: req.params.reactionId } } }, { runValidators: true, new: true })
+      .then((thought) => (!thought ? res.status(404).json({ message: "No thought found with that ID :(" }) : res.json(thought)))
+      .catch((err) => res.status(500).json(err));
+  },
 };
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,11 +1,17 @@
 const router = require("express").Router();
 
-const { postThought, getThoughts, getSingleThought, updateThought, deleteThought } = require("../../controllers/thoughtController");
+const { postThought, getThoughts, getSingleThought, updateThought, deleteThought, postReaction, deleteReaction } = require("../../controllers/thoughtController");
 
 // /api/thoughts
 router.route("/").get(getThoughts).post(postThought);
 
 // /api/thoughts/:thoughtId
 router.route("/:thoughtId").get(getSingleThought).put(updateThought).delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route("/:thoughtId/reactions").post(postReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
 
 module.exports = router;
