0cd7763ae7b06756b5b1e77f3d6e67e1d8d68754
Parker Mathis <parkerjmathis@gmail.com>
2023-02-14 18:05:04 -0800

    Added getter methods to format timestamps

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,11 +1,18 @@
 const { Schema, Types } = require("mongoose");
 
-const reactionSchema = new Schema({
-  reactionId: { type: Schema.Types.ObjectId, default: () => new Types.ObjectId() },
-  reactionBody: { type: String, required: true, maxlength: 280 },
-  username: { type: String, required: true },
-  //  TODO: Use a getter method to format the timestamp on query
-  createdAt: { type: Date, default: Date.now },
-});
+const reactionSchema = new Schema(
+  {
+    reactionId: { type: Schema.Types.ObjectId, default: () => new Types.ObjectId() },
+    reactionBody: { type: String, required: true, maxlength: 280 },
+    username: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now, get: (timestamp) => timestamp.toLocaleString() },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
 
 module.exports = reactionSchema;
--- models/Thought.js
+++ models/Thought.js
@@ -4,14 +4,14 @@ const reactionSchema = require("./Reaction");
 const thoughtSchema = new Schema(
   {
     thoughtText: { type: String, required: true, minlength: 1, maxlength: 280 },
-    //  TODO: Use a getter method to format the timestamp on query
-    createdAt: { type: Date, default: Date.now },
+    createdAt: { type: Date, default: Date.now, get: (timestamp) => timestamp.toLocaleString() },
     username: { type: String, required: true },
     // Array of nested documents created with the `reactionSchema`
     reactions: [reactionSchema],
   },
   {
     toJSON: {
+      getters: true,
       virtuals: true,
     },
     id: false,
